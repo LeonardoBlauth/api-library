9a143dc7800949769ce1086e0639c4bc72b9fc9f
LeoBlauth <leonardo.blauth@dealerup.com.br>
2021-10-13 09:04:40 -0300

    defining owners migration

---
+++ database/migrations/2021_10_13_120418_create_owners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnersTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('first_name')
+                ->nullable(false);
+
+            $table->string('last_name')
+                ->nullable(false);
+
+            $table->string('phone')
+                ->nullable(false);
+
+            $table->date('birth_date')
+                ->nullable(false);
+
+            $table->foreignId('address_id')
+                ->constrained('addresses', 'id')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('owners');
+    }
+}
