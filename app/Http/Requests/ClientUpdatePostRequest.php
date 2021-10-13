b0ee307ddb6600d0d1761c3db0f4d3a13733529e
LeoBlauth <leonardo.blauth@dealerup.com.br>
2021-10-13 18:39:00 -0300

    defining client update post request

---
+++ app/Http/Requests/ClientUpdatePostRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientUpdatePostRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'string|min:3',
+            'last_name' => 'string|min:3',
+            'phone' => 'string|size:15',
+            'birth_date' => 'date',
+            'rented_qty' => 'integer|max:10',
+            'address_id' => 'integer|exists:addresses,id',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'phone.size' => 'The phone must have 15 characters and must be in the format (00) 00000-0000'
+        ];
+    }
+}
