5f4ee69ce1b93db10282ee9bb10403bf02965065
LeoBlauth <leonardo.blauth@dealerup.com.br>
2021-10-14 19:42:07 -0300

    defining employee controller

---
+++ app/Http/Controllers/Gender/GenderController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Gender;
+
+use App\Http\Controllers\Traits\BaseDestroy\BaseDestroyController;
+use App\Http\Controllers\Traits\BaseShow\BaseShowController;
+
+class GenderController
+{
+    use BaseShowController, BaseDestroyController;
+}
