a7f782e7c9d692aeede0ff4433db51f55dbd88e7
LeoBlauth <leonardo.blauth@dealerup.com.br>
2021-10-14 19:43:35 -0300

    defining role controller

---
+++ app/Http/Controllers/Role/RoleController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Role;
+
+use App\Http\Controllers\Traits\BaseDestroy\BaseDestroyController;
+use App\Http\Controllers\Traits\BaseShow\BaseShowController;
+
+class RoleController
+{
+    use BaseShowController, BaseDestroyController;
+}
